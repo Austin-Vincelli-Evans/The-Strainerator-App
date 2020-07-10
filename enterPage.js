d83e0cb9fe93f9a4f31830a6b55c87e3bcf124da
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-07-10 11:04:11 -0600

    update content, navigation

--- enterPage.js
+++ enterPage.js
@@ -1,14 +1,70 @@
-const enterButton = document.getElementById('enter-button')
+const starterURL = `http://localhost:3000/starters`
+const flavorURL = `http://localhost:3000/flavors`
+const finaleURL = `http://localhost:3000/finales`
 
-enterButton.addEventListener('click', alertFunction)
 
-function alertFunction(event){ 
-    window.confirm('By Selecting OK\n you confirm you are either 21 years of age \nor you have a medical card')
-    if(confirm('You are older then 21 or have your med card!')){
-        window.location.replace(`http://localhost:3002/index.html`)
-    } else {
-        txt = "Come back another time"
-    }
+const starterList = document.getElementById('starter-list')
+const flavorList = document.getElementById('flavor-list')
+const finaleList = document.getElementById('finale-list')
+const generateButton = document.getElementById('generate')
+const clearButton = document.getElementById('clear-button')
+let starterWord = ""
+let flavorWord = ""
+let finaleWord = ""
 
-    
+generateButton.addEventListener('click', fetchCalls)
+
+
+function fetchCalls(){
+    fetch(starterURL)
+        .then(response => response.json())
+        .then(result => handleStarter(result))
+    fetch(flavorURL)
+        .then(response => response.json())
+        .then(result => handleFlavor(result))
+    fetch(finaleURL)
+        .then(response => response.json())
+        .then(result => handleFinale(result))
+}
+
+
+
+
+function handleStarter(starter){
+    starterWord = starter[Math.floor(Math.random() * starter.length)].name
+    renderStarter(starterWord)
+}
+
+function renderStarter(word){
+    const starterWord = document.createElement('li')
+    starterWord.innerText = word
+    starterList.append(starterWord)
+}
+
+
+function handleFlavor(flavor){
+    flavorWord = flavor[Math.floor(Math.random() * flavor.length)].name
+    renderFlavor(flavorWord)
+}
+
+function renderFlavor(word){
+    const flavorWord = document.createElement('li')
+    flavorWord.innerText = word
+    flavorList.append(flavorWord)
+}
+
+function handleFinale(finale){
+    finaleWord = finale[Math.floor(Math.random() * finale.length)].name
+    renderFinale(finaleWord)
+}
+
+function renderFinale(word){
+    const finaleWord = document.createElement('li')
+    finaleWord.innerText = word
+    finaleList.append(finaleWord)
+}
+function clearStrain(event){
+    starterList.remove(starterWord)
+    flavorList.remove(flavorWord)
+    finaleList.remove(finaleWord)
 }
--- index.js
+++ index.js
@@ -1,70 +1,14 @@
-const starterURL = `http://localhost:3000/starters`
-const flavorURL = `http://localhost:3000/flavors`
-const finaleURL = `http://localhost:3000/finales`
+const enterButton = document.getElementById('enter-button')
 
+enterButton.addEventListener('click', alertFunction)
 
-const starterList = document.getElementById('starter-list')
-const flavorList = document.getElementById('flavor-list')
-const finaleList = document.getElementById('finale-list')
-const generateButton = document.getElementById('generate')
-const clearButton = document.getElementById('clear-button')
-let starterWord = ""
-let flavorWord = ""
-let finaleWord = ""
+function alertFunction(event){ 
+    window.confirm('By Selecting OK you confirm you are either 21 years of age or you have a medical card')
+    if(confirm('You are older then 21 or have your med card!')){
+        window.location.replace(`http://localhost:3001/enterpage.html`)
+    } else {
+        txt = "Come back another time"
+    }
 
-generateButton.addEventListener('click', fetchCalls)
-
-
-function fetchCalls(){
-    fetch(starterURL)
-        .then(response => response.json())
-        .then(result => handleStarter(result))
-    fetch(flavorURL)
-        .then(response => response.json())
-        .then(result => handleFlavor(result))
-    fetch(finaleURL)
-        .then(response => response.json())
-        .then(result => handleFinale(result))
-}
-
-
-
-
-function handleStarter(starter){
-    starterWord = starter[Math.floor(Math.random() * starter.length)].name
-    renderStarter(starterWord)
-}
-
-function renderStarter(word){
-    const starterWord = document.createElement('li')
-    starterWord.innerText = word
-    starterList.append(starterWord)
-}
-
-
-function handleFlavor(flavor){
-    flavorWord = flavor[Math.floor(Math.random() * flavor.length)].name
-    renderFlavor(flavorWord)
-}
-
-function renderFlavor(word){
-    const flavorWord = document.createElement('li')
-    flavorWord.innerText = word
-    flavorList.append(flavorWord)
-}
-
-function handleFinale(finale){
-    finaleWord = finale[Math.floor(Math.random() * finale.length)].name
-    renderFinale(finaleWord)
-}
-
-function renderFinale(word){
-    const finaleWord = document.createElement('li')
-    finaleWord.innerText = word
-    finaleList.append(finaleWord)
-}
-function clearStrain(event){
-    starterList.remove(starterWord)
-    flavorList.remove(flavorWord)
-    finaleList.remove(finaleWord)
+    
 }
